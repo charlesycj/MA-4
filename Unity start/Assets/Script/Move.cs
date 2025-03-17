730c384d201f2c22fd6b0385b8b9beede4639339
ryoon114 <kilhoon114@gmail.com>
2025-03-17 16:03:20 +0900

    2025.03.17
    방향과 이동 코드 고침 이제 방향에 따라 앞으로 이동 가능

--- Unity start/Assets/Script/Move.cs
+++ Unity start/Assets/Script/Move.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public Transform Player; // 플레이어의 Transform
+    public Transform Player; 
     public float speed = 0.001f; // 이동 속도
     
-    [SerializeField] private int dice = 1; // 이동할 거리 (z 방향으로 1)
+    [SerializeField] private int dice = 1;
 
-     void Start()
+    void Start()
     {
-            StartCoroutine(move1());
+        StartCoroutine(move1());
+        Debug.Log(Player.forward);
     }
+   
+    void Update()
+    {
+  
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            StartCoroutine(move1());
+        }
+    }
+    
 
     private IEnumerator move1()
     {
@@ -29,7 +40,7 @@ public class Move : MonoBehaviour
         Vector3 target1 = startPos + new Vector3(0, 1, 0);
         while (Vector3.Distance(Player.position, target1) > 0.005f)
         {
-            Player.position = Vector3.MoveTowards(Player.position, target1, speed); // 이동
+            Player.position = Vector3.MoveTowards(Player.position, target1, speed * Time.deltaTime);
             yield return  null;
         }
         Player.position = target1;
@@ -37,12 +48,12 @@ public class Move : MonoBehaviour
     }
     private IEnumerator ExMove()
     {
-        Vector3 startPos = Player.position; // 시작 위치
-        Vector3 target = startPos + new Vector3(0, 0, 1);
+        Vector3 startPos = Player.position;
+        Vector3 target = startPos + Player.forward;
         while (Vector3.Distance(Player.position, target) > 0.005f)
         {
-            Player.position = Vector3.MoveTowards(Player.position, target, speed); // 이동
-            yield return null;
+            Player.position = Vector3.MoveTowards(Player.position, target, speed * Time.deltaTime);
+            yield return  null;
         }
         Player.position = target;
     }
@@ -52,7 +63,7 @@ public class Move : MonoBehaviour
         Vector3 target2 = startPos + new Vector3(0, -1, 0);
         while (Vector3.Distance(Player.position, target2) > 0.005f)
         {
-            Player.position = Vector3.MoveTowards(Player.position, target2, speed); // 이동
+            Player.position = Vector3.MoveTowards(Player.position, target2, speed * Time.deltaTime);
             yield return null;
         }
         Player.position = target2;
