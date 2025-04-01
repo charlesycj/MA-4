e646bb06d336954115cff1e097c3b7b0c2103829
ryoon114 <kilhoon114@gmail.com>
2025-04-01 17:49:51 +0900

    2025.04.01 17:49
    애니메이션 오류 고침

--- Unity start/Assets/Script/UIButton.cs
+++ Unity start/Assets/Script/UIButton.cs
@@ -7,9 +7,11 @@ public class UIButton : MonoBehaviour
     private bool isMoving = true;
     
 
+    
     public void OnMoveButtonClick()
     {
         isMoving = !isMoving; // 상태 반전
         panelAnimator.SetBool("isMoving", isMoving); // 애니메이션 트리거
     }
+   
 }
